7b2ddd0aa454807fc3dc07fd904d9ecd69381a55
BrunoZuffo <brunoalmeida442@yahoo.com>
2025-08-03 00:48:13 -0300

    estudando melhor python e scraping para o projeto

--- scraper.py
+++ scraper.py
@@ -12,6 +12,16 @@ if(resposta.status_code==200):
     print('Site acessado com sucesso!')
     html=resposta.content #armazena o conteúdo da url contido na resposta no html
     
-    #analisando o conteúdo
+    #analisando o html, agora todos os dados serão acessados através de soup
     soup=BeautifulSoup(html,'html.parser')
 
+    #buscando todas as colunas de: dia, almoço, jantar
+    dias=soup.find_all('td',class_='column-1')
+    almoco=soup.find_all('td',class_='column-2')
+    jantar=soup.find_all('td',class_='column-3')
+    
+    #imprimindo
+    for i in range(len(dias)):
+        dia = dias[i].text.strip()
+        if(dia):
+            print(f'dia: {dia}')
